b45f7bee34ca0e4648ca48a4f7b87fc027cf519f
Wendrowski <rasstrigin1349@gmail.com>
2022-02-09 23:17:22 +0300

    Remark

--- RAVESLI/Exercise18.cpp
+++ RAVESLI/Exercise18.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
-using namespace std;                                // используем пространство имён std
+using namespace std;                                
 int main()
 {
-	setlocale(LC_ALL, "rus");                        // используем кириллицу
+	setlocale(LC_ALL, "rus");        
 	int x, y;
 	cout << "Введите меньшее число : ";
 	cin >> x;
 	cout << "Введите большее число : ";
 	cin >> y;
 	if (x > y)
-	{                                               // начало вложенного блока
-		int z;                                      // объявляем переменную z во вложенном блоке
+	{                                               
+		int z;                                      
 		z = x;
 		x = y;
 		y = z;
-	}                                               // конец вложенного блока, z уничтожается здесь
+	}                                               
 	cout << "большее число : " << y << endl;
 	cout << "меньшее число : " << x << endl;
 	system("pause");
