8e13ad4315db507d56fbbee9e75668e74e5f99ea
Wendrowski <rasstrigin1349@gmail.com>
2022-02-09 23:18:12 +0300

    Remark

--- RAVESLI/Exercise17.cpp
+++ RAVESLI/Exercise17.cpp
@@ -5,21 +5,21 @@ int main()
 {
     setlocale(LC_ALL, "rus");
     cout << "Введите число: " << "\n";
-    double number1; //обьявление number1
+    double number1; 
     cin >> number1;
     cout << "Введите большее число" << "\n";
-    double number2; //обьявление number2
+    double number2; 
     cin >> number2;
     if (number1 > number2)
     {
-        double x = number1; //определяем х
+        double x = number1; 
         number1 = number2;
         number2 = x;
         cout << "Меняем значения местами" << "\n";
-    }//переменная х уничтожается
+    }
 
     cout << "Меньшее число: " << number1 << "\n";
     cout << "Большее число: " << number2 << "\n";
 
     return 0;
-} //переменные number1, number2 уничтожаются
+} 
