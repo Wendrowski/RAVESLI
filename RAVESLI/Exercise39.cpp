efbc4cbea394c8be771bc6ae3678d196cc88bf35
Wendrowski <rasstrigin1349@gmail.com>
2022-02-13 21:35:07 +0300

    Ex39

---
+++ RAVESLI/Exercise39.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int x;
+	cout << "Введите целое число X: ";
+	cin >> x;
+	cout << "Ваш X: " << x << endl;
+
+	int y;
+	cout << "Введите целое число Y которое больше X: ";
+	cin >> y;
+	if (y > x)
+		cout << "Ваш Y: " << y << " Больше чем X: " << x << endl;
+	else
+	{
+		cout << "Меняем значения местами" << endl;
+
+		int a(y);
+		cout << "X = " << a << endl;
+
+		int b(x);
+		cout << "Y = " << b << endl;
+	}
+
+
+	return 0;
+}
