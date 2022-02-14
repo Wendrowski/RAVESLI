304935455d37d61e0cf531fb5f78222adb5150e6
Wendrowski <rasstrigin1349@gmail.com>
2022-02-14 21:42:34 +0300

    Remark

--- RAVESLI/Exercise44.cpp
+++ RAVESLI/Exercise44.cpp
@@ -17,7 +17,7 @@ int main()
     Fruits fruit = LEMON; // Fruits и LEMON доступны в одной области видимости (добавлять префикс не нужно)
     Colors color = PINK; // Colors и PINK доступны в одной области видимости (добавлять префикс не нужно)
 
-    if (fruit == color) // компилятор будет сравнивать эти переменные как целые числа
+    if (fruit == color) 
         std::cout << "fruit and color are equal\n"; 
     else
         std::cout << "fruit and color are not equal\n";
