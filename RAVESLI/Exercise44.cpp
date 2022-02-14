8c7a00e63b0abc7c6c2d0c4c17bf70e99a695cd0
Wendrowski <rasstrigin1349@gmail.com>
2022-02-14 21:41:02 +0300

    Ex44

---
+++ RAVESLI/Exercise44.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    enum Fruits
+    {
+        LEMON, // LEMON находится внутри той же области видимости, что и Fruits
+        KIWI
+    };
+
+    enum Colors
+    {
+        PINK, // PINK находится внутри той же области видимости, что и Colors
+        GRAY
+    };
+
+    Fruits fruit = LEMON; // Fruits и LEMON доступны в одной области видимости (добавлять префикс не нужно)
+    Colors color = PINK; // Colors и PINK доступны в одной области видимости (добавлять префикс не нужно)
+
+    if (fruit == color) // компилятор будет сравнивать эти переменные как целые числа
+        std::cout << "fruit and color are equal\n"; // и обнаружит, что они равны!
+    else
+        std::cout << "fruit and color are not equal\n";
+
+    return 0;
+}
